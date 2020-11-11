b5a22fd9e477d994e53333c5b69b930546fb097e
Jędrzej Lewandowski <jedrzejblew@gmail.com>
2020-11-11 17:56:52 +0100

    feat: hero header

--- render_callbacks/client_post.php
+++ render_callbacks/client_post.php
@@ -1,20 +1,20 @@
 <?php
 function siejmy_tablogrid_client_post_render_callback( $block_attributes, $content ) {
     require_once( ABSPATH . 'wp-content/plugins/siejmycommon-plugin/classes/ImageRenderer.php');
-    require_once( ABSPATH . 'wp-content/plugins/siejmycommon-plugin/classes/TimeAgoRengerer.php');
+    require_once( ABSPATH . 'wp-content/plugins/siejmycommon-plugin/classes/HeroCaptionRenderer.php');
     wp_enqueue_style('siejmy-tablogrid-client-post');
 
     $post_id =$block_attributes['postId'];
 
     $out = '<div class="tablogrid_client tablopost">';
-          if(empty($post_id)) {
-            $out .= '<span>Wybierz numer ID wpisu w edytorze strony</span>';
-          }
-          else {
-            $out .= renderPost($post_id);
-          }
-        $out .= '</div>';
-        return  $out;
+    if(empty($post_id)) {
+      $out .= '<span>Wybierz numer ID wpisu w edytorze strony</span>';
+    }
+    else {
+      $out .= renderPost($post_id);
+    }
+    $out .= '</div>';
+    return  $out;
 }
 
 function renderPost($post_id) {
@@ -24,48 +24,22 @@ function renderPost($post_id) {
     }
 
     return renderPostContent($post);
-
-    return '' . renderImg($post) . '';
   }
 
   function renderPostContent($post) {
+    $imgRenderer = new ImageRenderer();
+    $captionRenderer = new HeroCaptionRenderer();
+
     $permalink = get_permalink($post);
     $mediaId     = get_post_thumbnail_id($post);
     $alt = $post->post_title;
-    $imgRenderer = new ImageRenderer();
 
-    $postHeroStyles = '<style>#posthero_'. $post->ID .' { background-image: url(' . $imgRenderer->getFallbackDataSrc($mediaId) . '); }</style>';
-    return '
-    <a class="featpost_a" href="' . $permalink . '" id="posthero_'. $post->ID .'">
-        ' . $imgRenderer->renderImgByAttachmentId($mediaId, $alt) . '
-        <div class="caption">
-          ' . renderTag($post) . '
-          <h3>' . $post->post_title . '</h3>
-          <div class="subline">
-            <span class="author">' . getAuthorName($post) . '</span>
-            ' . TimeAgoRengerer::getTimeAgoKatoPL($post, ' &nbsp;◉') . '
-          </div>
-        </div>
-      </a>
-      ' . $postHeroStyles;
-  }
-
-  function renderTag($post) {
-    $mainCategoryName = getMainCategoryName($post);
-    if(empty($mainCategoryName)) {
-      return '';
-    }
-    return '<div class="tag">' . $mainCategoryName . '</div>';
-  }
-
-  function getMainCategoryName($post) {
-    $categories = get_the_category($post->ID);
-    if(count($categories) == 0) {
-      return '';
-    }
-    return $categories[0]->name;
-  }
-
-  function getAuthorName($post) {
-    return get_the_author_meta('display_name', $post->post_author);
+    return $imgRenderer->renderImageHero(array(
+      'mediaId' => $mediaId,
+      'cssClass' => 'featpost_a',
+      'alt' => $alt,
+      'href' => $permalink,
+      'elementId' => 'tablopost_' . $post->ID,
+      'caption' => $captionRenderer->render($post),
+    ));
   }
