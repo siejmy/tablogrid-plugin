0c17a05a11c80e330a244828ec0861d36fe1ee5c
Jędrzej Lewandowski <jedrzejblew@gmail.com>
2021-12-11 21:53:53 +0100

    feat: more space for editing

--- build/index.asset.php
+++ build/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('wp-block-editor', 'wp-blocks', 'wp-components', 'wp-compose', 'wp-data', 'wp-element', 'wp-polyfill'), 'version' => 'e4d4b7cc034e5f18c7c7a3c79c2e4fc8');
+<?php return array('dependencies' => array('wp-block-editor', 'wp-blocks', 'wp-components', 'wp-compose', 'wp-data', 'wp-element', 'wp-polyfill'), 'version' => '4e96bab2da17a4294d266db4cc4d994d');
